6a5b278352d3f6c9995cb70719ee67cc226298cf
Ahmed-Galadi <galadiahmed99@gmail.com>
2025-04-17 05:58:02 +0000

    ex02 is finished working well

--- CPP_module_02/ex02/Fixed.cpp
+++ CPP_module_02/ex02/Fixed.cpp
@@ -4,30 +4,30 @@
 const int Fixed::numFracBits = 8;
 
 Fixed::Fixed(): fixedPoint(0) {
-	std::cout << "Default constructor called" << std::endl;
+	//std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(const int input) {
-	std::cout << "Int constructor called" << std::endl;
+	//std::cout << "Int constructor called" << std::endl;
 	this->fixedPoint = input << this->numFracBits;
 }
 
 Fixed::Fixed(const float input) {
-	std::cout << "Float constructor called" << std::endl;
+	//std::cout << "Float constructor called" << std::endl;
 	this->fixedPoint = roundf(input * (1 << this->numFracBits));
 }
 
 Fixed::Fixed(const Fixed &copy) {
-	std::cout << "Copy constructor called" << std::endl;
+	//std::cout << "Copy constructor called" << std::endl;
 	*this = copy;
 }
 
 Fixed::~Fixed() {
-	std::cout << "Destructor called" << std::endl;
+	//std::cout << "Destructor called" << std::endl;
 }
 
 Fixed &Fixed::operator=(const Fixed &src) {
-	std::cout << "Copy assignment operator called" << std::endl;
+	//std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &src)
 		this->fixedPoint = src.fixedPoint;
 	return *this;
@@ -67,6 +67,40 @@ float	Fixed::operator*(const Fixed &other) const {
 
 float	Fixed::operator/(const Fixed &other) const {
 	return (this->toFloat() / other.toFloat());
+}
+
+Fixed	Fixed::operator++(const int) {
+	Fixed oldThis = *this;
+	this->fixedPoint++;
+	return (oldThis);
+}
+
+Fixed	Fixed::operator++() {
+	++this->fixedPoint;
+	return (*this);
+}
+
+Fixed	Fixed::operator--(const int) {
+	Fixed oldThis = *this;
+	this->fixedPoint--;
+	return (oldThis);
+}
+
+Fixed Fixed::operator--(void) {
+	--this->fixedPoint;
+	return (*this);
+}
+
+Fixed Fixed::min(const Fixed &num1, const Fixed &num2) {
+	if (num1 <= num2)
+		return (num1);
+	return (num2);
+}
+
+Fixed Fixed::max(const Fixed &num1, const Fixed &num2) {
+	if (num1 >= num2)
+		return (num1);
+	return (num2);
 }
 
 int	Fixed::toInt(void) const {
--- CPP_module_02/ex02/Fixed.hpp
+++ CPP_module_02/ex02/Fixed.hpp
@@ -26,7 +26,11 @@ class Fixed {
 		bool	operator>=(const Fixed &other) const;
 		bool	operator<=(const Fixed &orther) const;
 		float	operator+(const Fixed &other) const;
+		Fixed	operator++(const int);
+		Fixed	operator++(void);
 		float	operator-(const Fixed &other) const;
+		Fixed	operator--(const int);
+		Fixed	operator--(void);
 		float	operator*(const Fixed &other) const;
 		float	operator/(const Fixed &other) const;
 	//public member functions
@@ -34,6 +38,9 @@ class Fixed {
 		int		toInt(void) const;
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
+	//static members
+		static Fixed	min(const Fixed &num1, const Fixed &num2);
+		static Fixed	max(const Fixed &num1, const Fixed &num2);
 };
 
 //overload insertion operator
