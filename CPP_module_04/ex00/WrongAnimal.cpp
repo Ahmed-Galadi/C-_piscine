0ed19f2aedeb3c320b55c321f376d048b40edab3
Ahmed-Galadi <galadiahmed99@gmail.com>
2025-04-18 16:22:11 +0000

    wrong animal class is finished

---
+++ CPP_module_04/ex00/WrongAnimal.cpp
@@ -0,0 +1,31 @@
+
+#include "WrongAnimal.hpp"
+
+WrongAnimal::WrongAnimal() : type("WrongAnimal") {
+    std::cout << "WrongAnimal default constructor is called!" << std::endl;
+}
+
+
+WrongAnimal::WrongAnimal(const WrongAnimal &other) {
+    *this = other;
+    std::cout << "WrongAnimal copy constructor is called!" << std::endl;
+}
+
+WrongAnimal & WrongAnimal::operator=(const WrongAnimal &other) {
+    if (this != & other) {
+        this->type = other.type;
+    }
+    return (*this);
+}
+
+WrongAnimal::~WrongAnimal() {
+    std::cout << "WrongAnimal is destroyed!" << std::endl;
+}
+
+void WrongAnimal::makeSound() const {
+    std::cout << "# This is WrongAnimal sound! #" << std::endl;
+}
+
+std::string WrongAnimal::getType() const {
+    return (type);
+}
