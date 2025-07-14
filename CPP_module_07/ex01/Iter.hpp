473eb4e9da9962297106f44ba99042458b5f1f06
Ahmed-Galadi <galadiahmed99@gmail.com>
2025-07-14 23:17:09 +0100

    cpp07/ex01 is finished

--- CPP_module_07/ex01/Iter.hpp
+++ CPP_module_07/ex01/Iter.hpp
@@ -9,15 +9,15 @@ for (int i = 0; i < length; i++)
 		function(array[i]);
 }
 
-template <typename T>
-void iter(const T* array, int length, void (*function)(const T&)) {
-	for (int i = 0; i < length; i++)
-		function(array[i]);
-}
-
 template<typename T>
 void	elementPlusTwo(T &elem) {
 	elem += 2;
+}
+
+template<typename T>
+void	elementPlusTwo(const T &elem) {
+	(void)elem;
+	std::cout << "Cannot Modify Const Element!" << std::endl;
 }
 
 template<typename T>
